0d336902b510b7d17fad56fcc26cca62040b3ac9
Takeru Ohta <phjgt308@gmail.com>
2017-10-24 10:29:41 +0900

    Update passage_transform.erl

--- src/passage_transform.erl
+++ src/passage_transform.erl
@@ -24,7 +24,7 @@
 %% ```
 %% foo(Bin) ->
 %%   try
-%%     passage_pd:start_span(foo, [{tags, #{application => example, module => example}}]),
+%%     passage_pd:start_span(foo, [{tags, #{application => example, module => example, foo => bar}}]),
 %%     passage_pd:set_tags(#{process => self(), size => byte_size(Bin)}),
 %%     <<"foo", Bin/binary>>
 %%    after
